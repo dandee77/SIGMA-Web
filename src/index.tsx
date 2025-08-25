2f9d9c1a7066e0844591b4474f8fa25381f18f86
Dandee Galang <dandeegalang.ph@gmail.com>
2025-08-25 18:06:56 +0800

    Replace main branch with new codebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
-import './index.css';
 import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./assets/globals.scss";
+import Redirect from "./components/Redirect.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <Redirect />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
